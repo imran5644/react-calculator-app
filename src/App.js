a2d5b206f0b7f5dcce931a78a023acae76a62ee6
imran5644 <imi56444@gmail.com>
2021-05-15 02:56:29 +0800

    Add the components folder and App file in it

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      Calculator-app
-    </div>
-  );
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="App">
+    Calculator Project
+  </div>
+);
+
+export default App;
