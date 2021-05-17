2abadc71e541b8e29ccb67a35166073c9b7cb4d9
imran5644 <imi56444@gmail.com>
2021-05-17 23:09:47 +0800

    Add the switch case for +, -, *, / in operate.js

---
+++ src/logic/operate.js
@@ -0,0 +1,39 @@
+import Big from 'big.js';
+
+const operate = (firstNum, secondNum, operation) => {
+    const bigFirstNum = Big(firstNum);
+    const bigSecondNum = Big(secondNum);
+
+    let result = {};
+    let total = 0;
+
+    switch(operation) {
+        case '+':
+            total = bigFirstNum + bigSecondNum;
+            result = { total, next: total, operation};
+            break;
+        
+        case '-':
+            total = bigFirstNum - bigSecondNum;
+            result = { total, next: total, operation};
+            break;
+
+        case '*':
+            total = bigFirstNum * bigSecondNum;
+            result = { total, next: total, operation};
+            break;
+
+        case '/':
+            total = bigFirstNum / bigSecondNum;
+            result = { total, next: total, operation};
+            break;
+        
+        default:
+            result = { bigFirstNum, bigSecondNum, operation};
+        break;
+    }
+      
+    return result;
+}
+
+export default operate;
