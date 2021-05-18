fec8f3a10a9450c21c9718ec6b76c6d87d2dedaa
imran5644 <imi56444@gmail.com>
2021-05-18 23:20:54 +0800

    Change App stateless component to stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
+import React, { Component } from 'react';
 
 calculate(4, 5, '+');
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends Component {
+   constructor(props) {
+     super(props);
+
+     this.state = {
+       total: null,
+       next: null,
+       operation: null,
+     };
+   }
+
+   render() {
+      return (
+        <>
+          <Display />
+          <ButtonPanel />
+        </>
+      );
+    }
+  } 
 
 export default App;
