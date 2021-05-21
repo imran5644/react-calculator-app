8c5b734b166f86b8ebcd1efd99dfac5130f1bed4
imran5644 <imi56444@gmail.com>
2021-05-21 23:45:24 +0800

    Add routes and background to main

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+const Home = () => (
+  <main>
+    <div>
+      <h1>Welcome to our page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aliquid, facere sapiente. Distinctio facilis dignissimos, ipsam vero qui
+        asperiores. Sapiente esse cupiditate saepe tempora, eum totam quibusdam
+        voluptatem quae labore aliquid?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Aliquid, facere sapiente. Distinctio facilis dignissimos, ipsam vero qui asperiores.
+        Sapiente esse cupiditate saepe tempora, eum totam quibusdam voluptatem quae labore
+        aliquid?
+      </p>
+    </div>
+  </main>
+);
+
+export default Home;
